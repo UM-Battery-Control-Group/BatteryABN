a30c250a3407afdfcb8dbc77e4d611c65771398b
YiLiiu <ziyiliu@umich.edu>
2024-11-25 17:02:56 +0000

    fix target_metadata

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from batteryabn.models.base import Base
+from batteryabn.extensions import db
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = db.Model.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
